9c43b7bfd82011334ffa89a1ef3c76198c50ba90
邱诗铭 <qiushiming1997@gmail.com>
2019-05-21 19:42:53 +0800

    feat: 引入 element-ui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import '@/common/scss/element-variable.scss'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
