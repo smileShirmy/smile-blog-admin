c06b32aba1409ccaa6f9d9c0a7024e22f86806c8
smileShirmy <qiushiming1997@gmail.com>
2019-07-09 08:36:55 +0800

    update: 添加环境

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ const Config = {
   sideBarLevel: 3, // 侧边栏层级限制, 3表示三级, 可设置 2 和 3
   defaultRoute: '/about', // 默认打开的路由
   // baseUrl: 'http://127.0.0.1:3000',
-  baseUrl: 'http://api.shirmy.me:3000',
+  baseUrl: process.env.VUE_APP_BASE_URL,
   notLoginRoute: ['login'], // 无需登录即可访问的路由 name
 }
 
